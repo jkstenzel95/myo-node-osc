82fa8cb019fcf2f2ccbd040c9088621f1bc7016b
sjacob95 <sjacob95@vt.edu>
2015-10-24 12:57:50 -0400

    Resuming work on this project... Broken still. Implemented command line arguments

--- myo-node-osc.js
+++ myo-node-osc.js
@@ -1,10 +1,17 @@
 var myo = require('myo');
 var osc = require('osc');
 
-// Change these as needed
-var OUT_PORT = 9090;
-var IN_PORT = 9091;
+var USAGE_STRING = "useage: node myo-node-osc.js [-h] [-test] [-n num_myos] [-in in_port] [-out out_port]\n\
+										\t-h: get usage info\n\
+										\t-test: enable testing mode\n\
+										\t-n num_myos: set the number of Myos to connect to\n\
+										\t-in in_port: set the port to receive OSC commands from\n\
+										\t-out out_port: set the port to send OSC commands over";
+
 var testing = false;
+var numMyos = 1;
+var outPort = 9090;
+var inPort = 9091;
 
 for (var i = 2; i < process.argv.length; i ++)
 {
@@ -13,85 +20,86 @@ for (var i = 2; i < process.argv.length; i ++)
 		case "-test":
 		{
 			testing = true;
+			console.log("Test mode enabled");
 			break;
+		}
+		case "-n":
+		{
+			if (process.argv.length == i + 1 ||  isNaN(process.argv[i + 1]))
+			{
+				console.log(USAGE_STRING);
+				return;
+			}
+			else
+			{
+				numMyos = Number(process.argv[++i]);
+				console.log("Number of Myos set to: ", numMyos);
+				break;
+			}
+		}
+		case "-in":
+		{
+			if (process.argv.length == i + 1 ||  isNaN(process.argv[i + 1]))
+			{
+				console.log(USAGE_STRING);
+				return;
+			}
+			else
+			{
+				inPort = Number(process.argv[++i]);
+				console.log("Receive port set to: ", inPort);
+				break;
+			}
+		}
+		case "-out":
+		{
+			if (process.argv.length == i + 1 ||  isNaN(process.argv[i + 1]))
+			{
+				console.log(USAGE_STRING);
+				return;
+			}
+			else
+			{
+				outPort = Number(process.argv[++i]);
+				console.log("Send port set to: ", outPort);
+				break;
+			}
+		}
+		case "-h":
+		{
+				console.log(USAGE_STRING);
+				return;
+		}
+		default:
+		{
+				console.log(USAGE_STRING);
+				return;
 		}
 	}
 }
 
-// BEGIN NITIALIZE PORTS
-var port = new osc.UDPPort({
-    localAddress: "127.0.0.1",
-    localPort: IN_PORT,
-    remoteAddress: "127.0.0.1",
-    remotePort: OUT_PORT
-});
-
-// END INITIALIZE PORTS
-
-// BEGIN UDP HANDLING
-	port.on('message', function(oscMessage)
-	{
-		console.log("RECEIVED: ", oscMessage);
-		//var oscTokens = oscMessage.split();
-	});
-
-	port.on('error', function(err){
-		console.log("ERROR\n", err);
-	});
-// END UDP HANDLING
-
 // BEGIN MYO HANDLING
 Myo.on('connected', function(){
 	this.setLockingPolicy("none");
-	console.log(this.arm, " Myo has connected!");
-	port.send({
-		address: "/Myo/" + this.arm + "/connected",
-		args: "1"
-	});
+	console.log(this.macAddress, " Myo has connected!");
 });
 
 Myo.on('disconnected', function()	{
-	console.log(this.arm, " Myo has disconnected!");
-	port.send({
-		address: "/Myo/" + this.arm + "/connected",
-		args: "0"
-	});
+	console.log(this.macAddress, " Myo has disconnected!");
 });
 
 Myo.on('arm_synced', function()	{
-	console.log(this.arm, " Myo has synced!");
-	port.send({
-		address: "/Myo/" + this.arm + "/synced",
-		args: "1"
-	});
+	console.log(this.macAddress, " Myo has synced!");
 });
 
 Myo.on('arm_unsynced', function()	{
-	console.log(this.arm, " Myo has unsynced!");
-	port.send({
-		address: "/Myo/" + this.arm + "/synced",
-		args: "0"
-	});
+	console.log(this.macAddress, " Myo has unsynced!");
 });
 
 Myo.on('imu', function(data)	{
 	var orientation = data.orientation;
 	var gyroscope = data.gyroscope;
 	var accelerometer = data.accelerometer;
-	port.send({
-		address: "/Myo/" + this.arm + "/imu/orientation",
-		args: [orientation.x, orientation.y, orientation.z, orientation.W]
-	}, "127.0.0.1", OUT_PORT
-	);
-	port.send({
-		address: "/Myo/" + this.arm + "/imu/gyroscope",
-		args: [gyroscope.x, gyroscope.y, gyroscope.z]
-	}, "127.0.0.1", OUT_PORT
-	);
-	port.send({
-		address: "/Myo/" + this.arm + "/imu/accelerometer",
-		args: [accelerometer.x, accelerometer.y, accelerometer.z]
-	});
 });
 
 Myo.on('pose', function(pose_name, edge)	{
@@ -100,15 +108,8 @@ Myo.on('pose', function(pose_name, edge)	{
 		return;
 	}
 	console.log(pose_name, " ", edge ? 1 : 0);
-	port.send({
-		address: "/Myo/" + this.arm + "/pose/" + pose_name,
-		args: edge
-	});
 });
 // END MYO HANDLING
-
-// BEGIN INITIALIZATION
-port.open();
 
 var Myo1 = myo.create();
 var Myo2 = myo.create();
